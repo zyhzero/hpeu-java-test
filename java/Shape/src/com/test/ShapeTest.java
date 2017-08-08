6d94712bc0607532b5966aabeafc25e1cf996dd6
Tangfancy <18311544668@163.com>
2017-08-08 14:28:32 +0800

    Update ShapeTest.java

--- java/Shape/src/com/test/ShapeTest.java
+++ java/Shape/src/com/test/ShapeTest.java
@@ -5,9 +5,12 @@ public class ShapeTest {
 	public static void main(String[] args) {
 
 		// 实例化一个对象
+		Shape shape = new Shape();
 		Circle circle = new Circle();
 		Rectangle rectangle = new Rectangle();
 		// 打印方法
+		shape.girth();
+		shape.area();
 		circle.girth();
 		circle.area();
 		rectangle.girth();
